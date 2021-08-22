9daa4066b792936209945ce095972954539a98ed
illustietor <illustietor@gmail.com>
2021-08-22 17:50:57 +0200

    Motes pokémon intercambiables

--- data/events/trades.asm
+++ data/events/trades.asm
@@ -9,14 +9,14 @@ TradeMons:
 	; "went and evolved" and also changed the trades to match. English
 	; Red/Blue uses the original JP Red/Green trades but with the JP Blue
 	; post-trade text.
-	db NIDORINO,   NIDORINA,  TRADE_DIALOGSET_CASUAL,    "TERRY@@@@@@"
+	db NIDORINO,   NIDORINA,  TRADE_DIALOGSET_CASUAL,    "BOB@@@@@@@@"
 	db ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL@@@@@"
 	db BUTTERFREE, BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "CHIKUCHIKU@" ; unused
-	db PONYTA,     SEEL,      TRADE_DIALOGSET_CASUAL,    "SAILOR@@@@@"
-	db SPEAROW,    FARFETCHD, TRADE_DIALOGSET_HAPPY,     "DUX@@@@@@@@"
-	db SLOWBRO,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "MARC@@@@@@@"
+	db PONYTA,     SEEL,      TRADE_DIALOGSET_CASUAL,    "MARINERO@@@"
+	db SPEAROW,    FARFETCHD, TRADE_DIALOGSET_HAPPY,     "KUMO@@@@@@@"
+	db SLOWBRO,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "MARCO@@@@@@"
 	db POLIWHIRL,  JYNX,      TRADE_DIALOGSET_EVOLUTION, "LOLA@@@@@@@"
 	db RAICHU,     ELECTRODE, TRADE_DIALOGSET_EVOLUTION, "DORIS@@@@@@"
-	db VENONAT,    TANGELA,   TRADE_DIALOGSET_HAPPY,     "CRINKLES@@@"
-	db NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "SPOT@@@@@@@"
+	db VENONAT,    TANGELA,   TRADE_DIALOGSET_HAPPY,     "RINKES@@@@@"
+	db NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "DON@@@@@@@@"
 	assert_table_length NUM_NPC_TRADES
